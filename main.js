8e4dc981bbe19c36da63cdb9a5863d147ba67d5c
Daniel Geymayer <dag0310@gmail.com>
2015-02-06 21:08:14 +0100

    Bugfix: Only reload data not the whole graph instance

--- main.js
+++ main.js
@@ -1,53 +1,53 @@
 (function() {
-'use strict';
+    'use strict';
 
-addToHomescreen();
+    addToHomescreen();
 
-function round(number, decimalPlaces) {
-    decimalPlaces = decimalPlaces ? decimalPlaces : 0;
-    var factor = Math.pow(10, decimalPlaces);
-    return Math.round(number * factor) / factor;
-}
-    
-function refresh() {
-    $.ajax({
-        type: 'GET',
-        cache: false,
-        url: 'log.php'
-    }).done(function(log) {
-        if (log.length === 0)
-            alert('There are no log entries yet.');
+    var chart = Morris.Line({
+        element: 'speedtest-chart',
+        data: [],
+        xkey: 'dateTime',
+        ykeys: ['ping', 'download', 'upload'],
+        labels: ['Ping [ms]', 'Down [Mbit/s]', 'Up [Mbit/s]'],
+        lineColors: ['rgb(21, 21, 234)', 'rgb(234, 27, 19)', 'rgb(230, 100, 19)'],
+        smooth: false,
+        resize: true,
+        continuousLine: true
+    });
 
-        var chartData = [];
-        for (var i = 0; i < log.length; i++) {
-            chartData.push({
-                dateTime: log[i][0],
-                ping:     round(log[i][1], 0),
-                download: round(log[i][2], 1),
-                upload:   round(log[i][3], 1)
-            });
-        }
+    function round(number, decimalPlaces) {
+        decimalPlaces = decimalPlaces ? decimalPlaces : 0;
+        var factor = Math.pow(10, decimalPlaces);
+        return Math.round(number * factor) / factor;
+    }
 
-        new Morris.Line({
-            element: 'speedtest-chart',
-            data: chartData,
-            xkey: 'dateTime',
-            ykeys: ['ping', 'download', 'upload'],
-            labels: ['Ping [ms]', 'Down [Mbit/s]', 'Up [Mbit/s]'],
-            lineColors: ['rgb(21, 21, 234)', 'rgb(234, 93, 19)', 'rgb(230, 156, 19)'],
-            smooth: false,
-            resize: true,
-            continuousLine: true
+    function refresh() {
+        $.ajax({
+            type: 'GET',
+            cache: false,
+            url: 'log.php'
+        }).done(function(log) {
+            if (log.length === 0)
+                alert('There are no log entries yet.');
+            var chartData = [];
+            for (var i = 0; i < log.length; i++) {
+                chartData.push({
+                    dateTime: log[i][0],
+                    ping:     round(log[i][1], 0),
+                    download: round(log[i][2], 1),
+                    upload:   round(log[i][3], 1)
+                });
+            }
+            chart.setData(chartData);
+        }).fail(function() {
+            alert('Log data could not be fetched :(');
         });
-    }).fail(function() {
-        alert('Log data could not be fetched :(');
-    });
-}
+    }
 
-$(function () {
-    $(window).focus(function() {
-        refresh()
+    $(function () {
+        $(window).focus(function() {
+            refresh()
+        });
+        refresh();
     });
-    refresh();
-});
 }());
