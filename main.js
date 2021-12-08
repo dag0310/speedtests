27e453ad3bf749322b3a1e6f1b597f09b27e65e7
Daniel Geymayer <dag0310@gmail.com>
2021-12-08 15:14:00 +0100

    Cap PING at 75 ms

--- main.js
+++ main.js
@@ -32,7 +32,7 @@
             for (var i = 0; i < log.length; i++) {
                 chartData.push({
                     dateTime: log[i][0],
-                    ping:     round(log[i][1], 0),
+                    ping:     round(Math.min(75, log[i][1]), 0),
                     download: round(log[i][2], 1),
                     upload:   round(log[i][3], 1)
                 });
